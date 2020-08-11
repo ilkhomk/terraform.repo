eddc991ee4ae1f1f6121c764aa73c40e448135fd
ilkhomk <i.kamolov1@gmail.com>
2020-08-11 15:59:39 +0000

    some changes

--- membership.tf
+++ membership.tf
@@ -2,15 +2,9 @@ resource "aws_iam_group_membership" "Developers_team" {
   name = "Developers-group-membership"
 
   users = [
-    "${aws_iam_user.Bob.name}",
+    "${aws_iam_user.Bob.name}", "${aws_iam_user.Tim.name}", "${aws_iam_user.Ben.name}",
   ]
-  users = [
-    "${aws_iam_user.Tim.name}",
-  ]
-  users = [
-    "${aws_iam_user.Ben.name}",
-  ]
-
+  
   group = "${aws_iam_group.Developers.name}"
 }
 resource "aws_iam_group_membership" "Management_team" {
