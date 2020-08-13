56bb5b92dcf84375b2d2b918cfa16f2d3e7b2d8e
ilkhomk <i.kamolov1@gmail.com>
2020-08-13 15:02:16 +0000

    some changes

--- groups.tf
+++ groups.tf
@@ -1,8 +1,8 @@
-resource "aws_iam_group" "Developers" {
-  name = "Developers"
-  path = "/"
-}
-resource "aws_iam_group" "Management" {
-  name = "Management"
-  path = "/"
-}
+# resource "aws_iam_group" "Developers" {
+#   name = "Developers"
+#   path = "/"
+# }
+# resource "aws_iam_group" "Management" {
+#   name = "Management"
+#   path = "/"
+# }
--- membership.tf
+++ membership.tf
@@ -1,18 +1,18 @@
-resource "aws_iam_group_membership" "Developers_team" {
-  name = "Developers-group-membership"
+# resource "aws_iam_group_membership" "Developers_team" {
+#   name = "Developers-group-membership"
 
-  users = [
-    "${aws_iam_user.Bob.name}", "${aws_iam_user.Tim.name}", "${aws_iam_user.Ben.name}",
-  ]
+#   users = [
+#     "${aws_iam_user.Bob.name}", "${aws_iam_user.Tim.name}", "${aws_iam_user.Ben.name}",
+#   ]
   
-  group = "${aws_iam_group.Developers.name}"
-}
-resource "aws_iam_group_membership" "Management_team" {
-  name = "Management-group-membership"
+#   group = "${aws_iam_group.Developers.name}"
+# }
+# resource "aws_iam_group_membership" "Management_team" {
+#   name = "Management-group-membership"
   
-  users = [
-    "${aws_iam_user.Ben.name}",
-  ]
+#   users = [
+#     "${aws_iam_user.Ben.name}",
+#   ]
   
-  group = "${aws_iam_group.Management.name}"
-}
+#   group = "${aws_iam_group.Management.name}"
+# }
